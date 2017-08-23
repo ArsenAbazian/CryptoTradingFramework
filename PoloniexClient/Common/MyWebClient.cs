458cd6f8bb0324776095e4b5d0ed256cd0da184f
Mike Rozhdestvenskiy <falcon@devexpress.com>
2017-08-23 14:14:15 -0700

    Added gzip to requests

--- PoloniexClient/Common/MyWebClient.cs
+++ PoloniexClient/Common/MyWebClient.cs
@@ -14,7 +14,9 @@ namespace CryptoMarketClient.Common {
             WebRequest res = base.GetWebRequest(address);
             HttpWebRequest hres = res as HttpWebRequest;
             if(hres != null) {
-                hres.ReadWriteTimeout = 3000;
+                hres.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
+                hres.Headers.Add(HttpRequestHeader.AcceptLanguage, "en-us");
+                hres.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
             }
             res.Timeout = 5000;
             return res;
