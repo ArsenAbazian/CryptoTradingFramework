32b96420158151f2a15d677e2bb36cd558a34154
fedor.safronov <fedor.safronov@devexpress.com>
2018-01-11 22:22:45 +0300

    wip 2. balances form tweaks

--- PoloniexClient/BalancesUserControl.Designer.cs
+++ PoloniexClient/BalancesUserControl.Designer.cs
@@ -27,6 +27,7 @@
             this.gridBalances = new DevExpress.XtraGrid.GridControl();
             this.viewBalances = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.colTicker = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.riHyperLinkEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemHyperLinkEdit();
             this.colCurrencyName = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colBalance = new DevExpress.XtraGrid.Columns.GridColumn();
             this.colAvailable = new DevExpress.XtraGrid.Columns.GridColumn();
@@ -36,6 +37,7 @@
             this.searchControl1 = new DevExpress.XtraEditors.SearchControl();
             ((System.ComponentModel.ISupportInitialize)(this.gridBalances)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.viewBalances)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.riHyperLinkEdit1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
             this.panelControl1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.searchControl1.Properties)).BeginInit();
@@ -47,6 +49,8 @@
             this.gridBalances.Location = new System.Drawing.Point(0, 42);
             this.gridBalances.MainView = this.viewBalances;
             this.gridBalances.Name = "gridBalances";
+            this.gridBalances.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.riHyperLinkEdit1});
             this.gridBalances.Size = new System.Drawing.Size(867, 553);
             this.gridBalances.TabIndex = 1;
             this.gridBalances.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
@@ -63,34 +67,78 @@
             this.viewBalances.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.None;
             this.viewBalances.GridControl = this.gridBalances;
             this.viewBalances.Name = "viewBalances";
-            this.viewBalances.OptionsBehavior.Editable = false;
+            this.viewBalances.OptionsBehavior.AlignGroupSummaryInGroupRow = DevExpress.Utils.DefaultBoolean.False;
+            this.viewBalances.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.False;
+            this.viewBalances.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.False;
+            this.viewBalances.OptionsBehavior.AllowFixedGroups = DevExpress.Utils.DefaultBoolean.False;
+            this.viewBalances.OptionsBehavior.AllowGroupExpandAnimation = DevExpress.Utils.DefaultBoolean.False;
+            this.viewBalances.OptionsBehavior.AllowPartialGroups = DevExpress.Utils.DefaultBoolean.False;
+            this.viewBalances.OptionsBehavior.AllowValidationErrors = false;
+            this.viewBalances.OptionsBehavior.AutoPopulateColumns = false;
+            this.viewBalances.OptionsBehavior.AutoSelectAllInEditor = false;
+            this.viewBalances.OptionsBehavior.AutoUpdateTotalSummary = false;
+            this.viewBalances.OptionsBehavior.EditorShowMode = DevExpress.Utils.EditorShowMode.MouseDown;
+            this.viewBalances.OptionsCustomization.AllowFilter = false;
             this.viewBalances.OptionsCustomization.AllowGroup = false;
+            this.viewBalances.OptionsCustomization.AllowMergedGrouping = DevExpress.Utils.DefaultBoolean.False;
+            this.viewBalances.OptionsCustomization.AllowRowSizing = true;
             this.viewBalances.OptionsFilter.AllowColumnMRUFilterList = false;
             this.viewBalances.OptionsFilter.AllowFilterEditor = false;
             this.viewBalances.OptionsFilter.AllowFilterIncrementalSearch = false;
             this.viewBalances.OptionsFind.AllowFindPanel = false;
+            this.viewBalances.OptionsMenu.EnableFooterMenu = false;
             this.viewBalances.OptionsMenu.EnableGroupPanelMenu = false;
+            this.viewBalances.OptionsMenu.ShowAddNewSummaryItem = DevExpress.Utils.DefaultBoolean.False;
+            this.viewBalances.OptionsMenu.ShowAutoFilterRowItem = false;
+            this.viewBalances.OptionsMenu.ShowDateTimeGroupIntervalItems = false;
+            this.viewBalances.OptionsMenu.ShowGroupSortSummaryItems = false;
+            this.viewBalances.OptionsMenu.ShowSplitItem = false;
             this.viewBalances.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.viewBalances.OptionsView.AllowHtmlDrawGroups = false;
             this.viewBalances.OptionsView.ShowFilterPanelMode = DevExpress.XtraGrid.Views.Base.ShowFilterPanelMode.Never;
+            this.viewBalances.OptionsView.ShowGroupExpandCollapseButtons = false;
             this.viewBalances.OptionsView.ShowGroupPanel = false;
             this.viewBalances.OptionsView.ShowIndicator = false;
             this.viewBalances.OptionsView.ShowVerticalLines = DevExpress.Utils.DefaultBoolean.False;
+            this.viewBalances.PopupMenuShowing += new DevExpress.XtraGrid.Views.Grid.PopupMenuShowingEventHandler(this.viewBalances_PopupMenuShowing);
+            this.viewBalances.DoubleClick += new System.EventHandler(this.viewBalances_DoubleClick);
             // 
             // colTicker
             // 
             this.colTicker.Caption = "Ticker";
+            this.colTicker.ColumnEdit = this.riHyperLinkEdit1;
             this.colTicker.FieldName = "CurrencyTicker";
             this.colTicker.Name = "colTicker";
+            this.colTicker.OptionsColumn.AllowGroup = DevExpress.Utils.DefaultBoolean.False;
+            this.colTicker.OptionsColumn.AllowIncrementalSearch = false;
+            this.colTicker.OptionsColumn.AllowMerge = DevExpress.Utils.DefaultBoolean.False;
+            this.colTicker.OptionsColumn.ShowInExpressionEditor = false;
             this.colTicker.Visible = true;
             this.colTicker.VisibleIndex = 0;
+            this.colTicker.Width = 144;
+            // 
+            // riHyperLinkEdit1
+            // 
+            this.riHyperLinkEdit1.AllowFocused = false;
+            this.riHyperLinkEdit1.AutoHeight = false;
+            this.riHyperLinkEdit1.Name = "riHyperLinkEdit1";
+            this.riHyperLinkEdit1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.riHyperLinkEdit1.SingleClick = true;
+            this.riHyperLinkEdit1.OpenLink += new DevExpress.XtraEditors.Controls.OpenLinkEventHandler(this.riHyperLinkEdit1_OpenLink);
             // 
             // colCurrencyName
             // 
             this.colCurrencyName.Caption = "Currency";
+            this.colCurrencyName.ColumnEdit = this.riHyperLinkEdit1;
             this.colCurrencyName.FieldName = "CurrencyName";
             this.colCurrencyName.Name = "colCurrencyName";
+            this.colCurrencyName.OptionsColumn.AllowGroup = DevExpress.Utils.DefaultBoolean.False;
+            this.colCurrencyName.OptionsColumn.AllowIncrementalSearch = false;
+            this.colCurrencyName.OptionsColumn.AllowMerge = DevExpress.Utils.DefaultBoolean.False;
+            this.colCurrencyName.OptionsColumn.ShowInExpressionEditor = false;
             this.colCurrencyName.Visible = true;
             this.colCurrencyName.VisibleIndex = 1;
+            this.colCurrencyName.Width = 144;
             // 
             // colBalance
             // 
@@ -99,8 +147,14 @@
             this.colBalance.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
             this.colBalance.FieldName = "Balance";
             this.colBalance.Name = "colBalance";
+            this.colBalance.OptionsColumn.AllowEdit = false;
+            this.colBalance.OptionsColumn.AllowGroup = DevExpress.Utils.DefaultBoolean.False;
+            this.colBalance.OptionsColumn.AllowIncrementalSearch = false;
+            this.colBalance.OptionsColumn.AllowMerge = DevExpress.Utils.DefaultBoolean.False;
+            this.colBalance.OptionsColumn.ShowInExpressionEditor = false;
             this.colBalance.Visible = true;
             this.colBalance.VisibleIndex = 2;
+            this.colBalance.Width = 144;
             // 
             // colAvailable
             // 
@@ -109,8 +163,14 @@
             this.colAvailable.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
             this.colAvailable.FieldName = "Available";
             this.colAvailable.Name = "colAvailable";
+            this.colAvailable.OptionsColumn.AllowEdit = false;
+            this.colAvailable.OptionsColumn.AllowGroup = DevExpress.Utils.DefaultBoolean.False;
+            this.colAvailable.OptionsColumn.AllowIncrementalSearch = false;
+            this.colAvailable.OptionsColumn.AllowMerge = DevExpress.Utils.DefaultBoolean.False;
+            this.colAvailable.OptionsColumn.ShowInExpressionEditor = false;
             this.colAvailable.Visible = true;
             this.colAvailable.VisibleIndex = 3;
+            this.colAvailable.Width = 144;
             // 
             // colPending
             // 
@@ -119,8 +179,14 @@
             this.colPending.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
             this.colPending.FieldName = "Pending";
             this.colPending.Name = "colPending";
+            this.colPending.OptionsColumn.AllowEdit = false;
+            this.colPending.OptionsColumn.AllowGroup = DevExpress.Utils.DefaultBoolean.False;
+            this.colPending.OptionsColumn.AllowIncrementalSearch = false;
+            this.colPending.OptionsColumn.AllowMerge = DevExpress.Utils.DefaultBoolean.False;
+            this.colPending.OptionsColumn.ShowInExpressionEditor = false;
             this.colPending.Visible = true;
             this.colPending.VisibleIndex = 4;
+            this.colPending.Width = 144;
             // 
             // panelControl1
             // 
@@ -174,6 +240,7 @@
             this.Load += new System.EventHandler(this.BalancesUserControl_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gridBalances)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.viewBalances)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.riHyperLinkEdit1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
             this.panelControl1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.searchControl1.Properties)).EndInit();
@@ -193,5 +260,6 @@
         private DevExpress.XtraEditors.PanelControl panelControl1;
         private DevExpress.XtraEditors.CheckButton chkAutoRefresh;
         private DevExpress.XtraEditors.SearchControl searchControl1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemHyperLinkEdit riHyperLinkEdit1;
     }
 }
--- PoloniexClient/BalancesUserControl.cs
+++ PoloniexClient/BalancesUserControl.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CryptoMarketClient.Bittrex;
+using DevExpress.XtraEditors.Controls;
+using DevExpress.XtraGrid.Views.Grid;
 
 namespace CryptoMarketClient {
     public partial class BalancesUserControl : UserControl {
@@ -30,6 +32,21 @@ namespace CryptoMarketClient {
 
         async void OnRefreshTimerTick(object sender, EventArgs e) {
             await Task<bool>.Factory.StartNew(() => Exchange.UpdateBalances(), TaskCreationOptions.LongRunning);
+        }
+
+        void riHyperLinkEdit1_OpenLink(object sender, OpenLinkEventArgs e) {
+            e.Handled = true;
+        }
+
+        void viewBalances_DoubleClick(object sender, EventArgs e) {
+
+        }
+
+        void viewBalances_PopupMenuShowing(object sender, PopupMenuShowingEventArgs e) {
+            if (e.MenuType == GridMenuType.Column) {
+                e.Menu.Items.RemoveAt(3);
+                e.Menu.Items.RemoveAt(2);
+            }
         }
     }
 }
