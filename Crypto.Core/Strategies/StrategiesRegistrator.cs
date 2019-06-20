c0f8426596c571297f80c8f13d0f3451f41236e1
Abazyan <ArsenAbazyan@gmail.com>
2019-06-20 18:25:38 +0300

    media

--- Crypto.Core/Strategies/StrategiesRegistrator.cs
+++ Crypto.Core/Strategies/StrategiesRegistrator.cs
@@ -23,15 +23,15 @@ namespace Crypto.Core.Strategies {
         }
         static List<StrategyRegistrationInfo> RegisterStrategies() {
             List<StrategyRegistrationInfo> list = new List<StrategyRegistrationInfo>();
-            list.Add(new StrategyRegistrationInfo() { Type = typeof(SimpleBuyLowSellHighStrategy), Group = StrategyGroup.Simple, Name = "Simple Buy Low Sell High", Description = "No Rocket Scinece, Just buy low and sell high. Thats it." });
+            //list.Add(new StrategyRegistrationInfo() { Type = typeof(SimpleBuyLowSellHighStrategy), Group = StrategyGroup.Simple, Name = "Simple Buy Low Sell High", Description = "No Rocket Scinece, Just buy low and sell high. Thats it." });
             list.Add(new StrategyRegistrationInfo() { Type = typeof(StatisticalArbitrageStrategy), Group = StrategyGroup.Arbitrage, Name = "Statistical Arbitrage", Description = "Simple Statistical Arbitrage Strategy for Trading Pairs (usually base currency and futures)." });
-            list.Add(new StrategyRegistrationInfo() { Type = typeof(SignalNotificationStrategy), Group = StrategyGroup.Signal, Name = "Signal Notification", Description = "This strategy just send notification when corresponding param's values found" });
+            //list.Add(new StrategyRegistrationInfo() { Type = typeof(SignalNotificationStrategy), Group = StrategyGroup.Signal, Name = "Signal Notification", Description = "This strategy just send notification when corresponding param's values found" });
             list.Add(new StrategyRegistrationInfo() { Type = typeof(TripleRsiIndicatorStrategy), Group = StrategyGroup.Signal, Name = "Triple Rsi Indicator", Description = "This strategy based on triple rsi indicators values." });
             list.Add(new StrategyRegistrationInfo() { Type = typeof(MacdTrendStrategy), Group = StrategyGroup.Signal, Name = "Macd Trend", Description = "This strategy based on Macd trend." });
-            list.Add(new StrategyRegistrationInfo() { Type = typeof(CustomTickerStrategy), Group = StrategyGroup.Custom, Name = "Custom Strategy", Description = "Your own fully customizable strategy" });
-            list.Add(new StrategyRegistrationInfo() { Type = typeof(MarketMakingStrategy), Group = StrategyGroup.Custom, Name = "Stupid Market Making Strategy", Description = "No Comments" });
-            list.Add(new StrategyRegistrationInfo() { Type = typeof(TriplePairStrategy), Group = StrategyGroup.Arbitrage, Name = "Triple Pair Strategy", Description = "" });
-            list.Add(new StrategyRegistrationInfo() { Type = typeof(TaSimpleStrategy), Group = StrategyGroup.Custom, Name = "Support Resistance Simple Breaks Strategy", Description = "-" });
+            //list.Add(new StrategyRegistrationInfo() { Type = typeof(CustomTickerStrategy), Group = StrategyGroup.Custom, Name = "Custom Strategy", Description = "Your own fully customizable strategy" });
+            //list.Add(new StrategyRegistrationInfo() { Type = typeof(MarketMakingStrategy), Group = StrategyGroup.Custom, Name = "Stupid Market Making Strategy", Description = "No Comments" });
+            list.Add(new StrategyRegistrationInfo() { Type = typeof(TriplePairStrategy), Group = StrategyGroup.Arbitrage, Name = "Triple Pair Strategy", Description = "Strategy based on tripe pair arbitrage" });
+            list.Add(new StrategyRegistrationInfo() { Type = typeof(TaSimpleStrategy), Group = StrategyGroup.TecnicalAnylysis, Name = "Ta Simple Strategy", Description = "Strategy based on simple technical analysis" });
             return list;
         }
     }
@@ -53,6 +53,7 @@ namespace Crypto.Core.Strategies {
         MarketMaking = "Market Making",
         Grid = "Grid",
         Signal = "Signal Notificators",
-        Custom = "Custom";
+        Custom = "Custom",
+        TecnicalAnylysis = "Technical Analysis";
     }
 }
