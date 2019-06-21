a32cc89ad474aa2d340904547d8ce87e95ebd936
Abazyan <ArsenAbazyan@gmail.com>
2019-06-21 10:28:57 +0300

    refactor

---
+++ Crypto.UI/Strategies/Custom/TaSimpleStrategyConfigurationControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CryptoMarketClient.Strategies.Custom;
+using Crypto.Core.Strategies;
+
+namespace Crypto.UI.Strategies.Custom {
+    public partial class TaSimpleStrategyConfigurationControl : CustomStrategyConfigurationControl {
+        public TaSimpleStrategyConfigurationControl() {
+            InitializeComponent();
+            this.colOrderBookDepth.Visible = false;
+            this.colUseOrderBook.Visible = false;
+            this.colUseKline.Visible = false;
+            this.colUseTradeHistory.Visible = false;
+            this.colSimulationDataFile.Visible = false;
+        }
+        protected override TickerInputInfo CreateDefaultTickerInputInfo() {
+            TickerInputInfo res = base.CreateDefaultTickerInputInfo();
+            res.UseKline = true;
+            res.UseOrderBook = true;
+            res.UseTradeHistory = true;
+            return res;
+        }
+    }
+}
